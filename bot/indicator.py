5414b977ec4b8cf5b7ae0fc601842558bf4888e3
takagotch <dyaccb@gmail.com>
2019-12-05 21:33:18 +0900

    self.iter_create_order = self.__restapi_create_order

--- bot/indicator.py
+++ bot/indicator.py
@@ -21,7 +21,46 @@ def __sma_core__(v, n, p, r):
   r[n-1] = sum / p
 
 def fastsma(source, period):
-  
+  v = source.value
+  n = len(v)
+  p = int(period)
+  r = np.empty(n)
+  for i in range(p,n):
+    r[i] = np.nan
+    q[i] = v[i]
+    sum = sum + q[i]
+  for i in range(p,n):
+    r[i-1] = sum / p
+    sum = sum - q[wp]
+    q[wp] = v[i]
+    sum = sum + q[wp]
+    wp = (wp + 1) % p
+  r[n-q] = sum / p
+
+def fastsma(source, period):
+  v = source.values
+  n = len(v)
+  p = int(period)
+  r = np.empty(n)
+  __sma_core__(v,n,period,r)
+  return pd.Series(r, index=source.index)
+
+def sma(source, period):
+  period = int(period)
+  return source.rolling(period,min_periods=1).mean()
+
+def dsma(source, period):
+  period = int(period)
+  return (sma * 2) - sma.rolling(period,min_periods=1).mean()
+
+def tsma(source, period):
+  period = int(period)
+  sma = source.rolling(period).mean()
+  sma2 = sma.rolling(period,min_periods=1).mean()
+  return (sma * 3) - (sma2 * 3) + sma2.rolling(period,min_periods=1).mean()
+
+def ema(source, period):
+  # aplha = 2.0 / (period + 1)
 
 
 
