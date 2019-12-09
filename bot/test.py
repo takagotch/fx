87565d4507b526a87d00bd468fbb9359f708d178
takagotch <dyaccb@gmail.com>
2019-12-10 06:29:47 +0900

    argv.exchange: argv.exchange != 'ehtocean' and argv.exchange != 'theocean1': exchange = exchanges[argv.exchange] symbol = argv.symbol if hasattr(exchange, 'skip') and exchange.skip: dump(green(exchange.id), 'skipped') else: if symbol: load_exchange(exchange) test(exchange, symbol) else: try_all_proxies(exchange, proxies)

--- bot/test.py
+++ bot/test.py
@@ -91,30 +91,219 @@ def test_ohlcv(exchange, symbol):
   else:
     dump(yellow(exchange.id), 'fetching OHLCV not supported')
 
+def test_tickers(exchange, symbol):
+  ignored_exchanges = [
+    'digifinex',        
+  ]
+  if exchange.id in ignored_exchanges:
+    return
+  if exchange.has['fetchTickers']:
+    delay = int(exchange.rateLimit / 1000)
+    time.sleep(delay)
+    tickers = None
+    try:
+      # dump(green(exchange.id), 'fetching all tickers at once...')
+      tickers = exchange.fetch_tickers()
+      dump(green(exchange.id), 'fetched all', green(len(list(tickers.keys()))), 'tickers')
+    except Exception as e:
+      dump(green(exchange.id), 'failed to fetch all tickers, fetching multiple tikcers at once...')
+      tickers = exchange.fetch_tickers([symbol])
+      dump(green(exchange.id), 'fetched', green(len(list(tickers.keys()))), 'tickers')
+
+def get_active_symbols(exchange):
+  return [symbol for symbol in exchange.symbols if is_active_symbol(exchange, symbol)]
+
+def is_active_symbol(exchange, symbol):
+  return ('.' not in symbol) and (('active' not in exchange.markets[symbol]) or (exchange.markets[symbol]['active']))
+
+def test_ticker(exchange, symbol):
+  ignored_exchange = [
+    'digifinex',
+  ]
+  if exchange.id in ignored_exchanges:
+    return
+  if exchange.has['fetchTicker']:
+    delay = int(exchange.rateLimit / 1000)
+    time.sleep(delay)
+    ticker = exchange.fetch_ticker(symbol)
+    dump(
+      green(exchange.id),
+      green(symbol),
+      'ticker',
+      ticker['datetime'],
+      'high: ' + str(ticker['high']),
+      'low: ' + str(ticker['low']),
+      'bid: ' + str(ticker['bid']),
+      'ask: ' + str(ticker['ask']),
+      'volume: ' + str(ticker['quoteVolume']))
+  else:
+    dump(green(exchange.id), green(symbol), 'fetch_ticker() not supported')
+
+def test_trades(exchange, symbol):
+  if exchange.has['fetchTrades']:
+    delay = int(exchange.rateTradeLimit / 1000)
+    time.sleep(delay)
+    # dump(green(exchange.id), green(symbol), 'fetching trades...')
+    trades = exchange.fetch_trades(symbol)
+    dump(green(exchange.id), green(symbol), 'fetched', green(len(list(trades))), 'trades')
+
+def test_symbol(exchange, symbol):
+  dump(green('SYMBOL: ' + symbol))
+  test_ticker(exchange, symbol)
+
+  if exchange.id == 'coinmarketcap':
+    response = exchange.fetchGlobal()
+    dump(green(response))
+  else:
+    test_order_book(exchange, symbol)
+    test_trades(exchange, symbol)
+
+  test_tickers(exchange, symbol)
+  test_ohlcv(exchange, symbol)
+
+def load_exchange(exchange):
+  exchange.load_markets()
+
+def test_exchange(exchange):
+
+  dump(green('EXCHANGE: ' + exchange.id))
+  # delay = 2
+  keys = list(exchange.markets.keys())
+
+  symbol = keys[0]
+  symbols = [
+    'BTC/USD',
+    'BTC/USDT',
+    'BTC/CNY',
+    'BTC/EUR',
+    'BTC/ETH',
+    'ETH/BTC',
+    'BTC/JPY',
+    'LTC/BTC',
+    'USD/SLL',
+  ]
+
+  for s in symbols:
+    if s in keys:
+      symbol = s
+      break
+
+  if symbol.find('.d') < 0:
+    test_symbol(exchange, symbol)
+
+  if (not hasattr(exchange, 'apiKey') or (len(exchange.apiKey) < 1)):
+    return
+
+  #
+
+  exchange.fetch_balance()
+  dump(green(exchange.id), 'fetched balance')
+
+  time.sleep(exchange.rateLimit / 1000)
+
+  if exchange.has['fetchOrders']:
+    try:
+      orders = exchange.fetch_orders(symbol)
+      dump(green(exchange.id), 'fetched', green(str(len(orders))), 'orders')
+    except (ccxt.ExchangeError, ccxt.NotSupported) as e:
+      dump_error(yellow('[' + type(e).__name__ + ']'), e.args)
+    # except ccxt.NotSupported as e:
+    #   dump(yellow(type(e).__name__), e.args)
+  
+  # time.sleep(delay)
+
+  # amount = 1
+  # price = 0.0161
+
+  # marketBuy = exchange.create_market_buy_order(symbol, amount)
+  # print(marketBuy)
+  # time.sleep(delay)
+
+  # marketSell = exchange.create_market_sell_order(symbol, amount)
+  # price(marketSell)
+  # time.sleep(delay)
+
+  # limitBuy = exchange.create_limit_buy_order(symbol, amount, price)
+  # print(limitSell)
+  # time.sleep(delay)
+
+def try_all_proxies(exchange, proxies=['']):
+  current_proxy = 0
+  max_retries = len(proxies)
+
+  if exchange.id == 'ccex' and max_retries > 1:
+    current_proxy = 1
+  if exchange.proxy in proxies:
+    current_proxy = proxies.index(exchange.proxy)
+  for num _retries in range(0, max_retries):
+    try:
+      exchange.proxy = proxies[current_proxy]
+      dump(green(exchange.id), 'using proxy', '`' + exchange.proxy + '`')
+      current_proxy = (current_proxy + 1) % len(proxies)
+      load_exchange(exchange)
+      test_exchange(exchange)
+
+    except (ccxt.RequestTimeout, ccxt.AuthenticationError, ccxt.NotSupported, ccxt.DDoSProtection, ccxt.ExchangeNotAvailable, ccxt.Exchange   ):
+        print({'type': type(e).__name__, 'num_retries': num_retries, 'max_retries': max_retries}, str(e)[0:200])
+      if (num_retries + 1) == max_retries:
+        dump_error(yello('[' + type(e).__name__ + ']'), str(e)[0:200])
+
+    else:
+      return True
+
+  return False
+
+proxies = [
+  '',
+  'https://cors-anywhere.herokuapp.com/',
+  # 'https://crossorigin.me/',
+]
+
+keys_folder = os.path.dirname(root)
+keys_global = os.path.join(keys_folder, 'keys.json')
+keys_local = os.path.join(keys_folder, 'keys.local.json')
+keys_file = keys_local if os.path.exists(keys_local) else keys_global
+
+with open(keys_file) as file:
+  config = json.load(file)
+
+for id in ccxt.exchanges:
+  if id == 'theocean' or id == 'theocean1':
+    continue
+  exchange = getattr(ccxt, id)
+  exchange_config = {'verbose': argv.verbose}
+  if sys.version_info[0] < 3:
+    exchange_config.update({'enableRateLimit': True})
+  if id in config:
+    exchange_config = ccxt.Exchange.deep_extend(exchange_config, config[id])
+  exchanges[id] = exchange(exchange_config)
 
 
+def main():
+  
+  if argv.exchange:
 
+    if argv.exchange != 'theocean' and argv.exchange != 'theocean1':
+      exchange = exchanges[argv.exchange]
+      symbol = argv.symbol
 
+      if hasattr(exchange, 'skip') and exchange.skip:
+        sump(green(exchange.id), 'skipped')
+      else:
+        if symbol:
+          load_exchange(exchange)
+          test_symbol(exchange, symbol)
+        else:
+          try_all_proxies(exchange, proxies)
+  else:
+    for exchange in sorted(exchanges.values(), key=lambda x: x.id):
+      if hasattr(exchange, 'skip') and exchange.skip:
+        dump(green(exchange.id), 'skipped')
+      else:
+        try_all_proxies(exchange, proxies)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+  main()
 
 
 
